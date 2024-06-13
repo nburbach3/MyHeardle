a82b3b60e8503d7a7c63026dbb8fc192a98c9fd1
Nick Burbach <nburbach@huskers.unl.edu>
2024-06-12 21:47:42 -0500

    Bug fix and add helper icons

--- src/main/java/com/burbach/MyHeardle/config/MyHeardleConfiguration.java
+++ src/main/java/com/burbach/MyHeardle/config/MyHeardleConfiguration.java
@@ -2,8 +2,13 @@ package com.burbach.MyHeardle.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class MyHeardleConfiguration {
@@ -11,11 +16,18 @@ public class MyHeardleConfiguration {
     private final static int size = 16 * 1024 * 1024;
     @Bean
     public WebClient webClient() {
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("My Heardle")
+                .maxConnections(500)
+                .pendingAcquireTimeout(Duration.ofSeconds(45))
+                .maxIdleTime(Duration.ofSeconds(600))
+                .build();
+        HttpClient client = HttpClient.create(connectionProvider);
         ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
                 .build();
         return WebClient.builder()
                .exchangeStrategies(strategies)
+                .clientConnector(new ReactorClientHttpConnector(client))
                .build();
     }
 }
